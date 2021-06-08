b1d27494362c000710efd46fd2fc8d4c73a5c097
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-06-08 05:50:06 -0400

    on user creation, email fields must be unique by case-insensitivity.  domain user enters for email address must be real, i.e., must exist or validator will fail creation of user

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,7 +2,14 @@ import hashlib
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
+import logging
+logger = logging.getLogger(__name__)
+from django.utils.translation import gettext as _
+# Note: we need dnspython for this to work
+import dns.resolver, dns.exception
+from django.utils.safestring import mark_safe
 # Custom User class which extends built-in User. Presently, just adds a "bio"
 # and a gravatar method. Feel free to add your own new fields here!
 #https://stackoverflow.com/questions/36330677/django-model-set-default-charfield-in-lowercase
@@ -13,12 +20,43 @@ class UserNameField(models.CharField):
     def get_prep_value(self, value):
         return str(value).lower()
 
+class UserEmailField(models.EmailField):
+    def __init__(self, *args, **kwargs):
+        super(UserEmailField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):    
+        return str(value).lower()
+
+# checks if domain user enters for email field is a real functioning domain
+def checkDomainExists(email):
+        domain = email.split('@')[1]
+
+        # Make sure the domain exists
+        try:
+            logger.debug('Checking domain %s', domain)
+
+            results = dns.resolver.query(domain, 'MX')
+
+        except dns.exception.DNSException as e:
+            logger.debug('Domain %s does not exist.', e)
+
+            raise \
+                ValidationError(mark_safe("The domain %s could not be found. <br/> Please enter a real email address."
+                                      % domain))
+                # ValidationError(("The domain %s could not be found.  Enter a real email address.") 
+                #                       % domain)
+                
+
+        return email
+
+ 
 
 class User(AbstractUser):
 
     bio = models.TextField()
     #username = models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
     username = UserNameField(max_length=200, unique=True)
+    email = UserEmailField(unique=True, validators=[EmailValidator])
 
     def gravatar(self, size=None):
         GRAVATAR_URL = 'https://gravatar.com/avatar/%s?d=identicon%s'
@@ -31,5 +69,7 @@ class User(AbstractUser):
             size_str = ''
 
         return GRAVATAR_URL % (digest, size_str)
+    def clean(self):
+        checkDomainExists(self.email)
 
  
