46af2b5407b03fa1119714351e383954dbe214dc
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-06-29 06:58:25 -0400

    fixed faulty migration

--- apps/github_dashboards/migrations/0008_auto_20210609_0332.py
+++ apps/github_dashboards/migrations/0008_auto_20210609_0332.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('github_dashboards', '0007_alter_dashboardpanel_creator'),
+        ('github_dashboards', '0006_panelscollection'),
     ]
 
     operations = [
